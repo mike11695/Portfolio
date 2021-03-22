262d7268129d37e74e66af621903f4595582cf74
mike1695 <luigixmario11695@gmail.com>
2021-03-22 18:32:55 -0400

    Set email settings for the contact form to work

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -134,3 +134,10 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'malopez1195@gmail.com'
+EMAIL_HOST_PASSWORD = 'timmythecat36'
