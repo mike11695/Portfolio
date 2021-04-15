18caf7d1c084490e5205f060f024317ee2fa92a2
mike1695 <luigixmario11695@gmail.com>
2021-04-14 21:07:25 -0400

    Added image slideshow for PetTP project

--- portfolio/pf/static/js/myjs.js
+++ portfolio/pf/static/js/myjs.js
@@ -2,40 +2,38 @@
 //https://www.w3schools.com/howto/howto_js_slideshow.asp
 
 $(document).ready(function() {
-  console.log("Yeet");
-
-  var curImage = 1;
-  displayImg(curImage);
+  var curImageLI = 1;
+  displayImgLI(curImageLI);
 
   //Go to next image to right or left
-  document.getElementById("left-arrow-btn").addEventListener("click", imageScrollLeft, false);
-  document.getElementById("right-arrow-btn").addEventListener("click", imageScrollRight, false);
+  document.getElementById("left-arrow-btn-li").addEventListener("click", imageScrollLeftLI, false);
+  document.getElementById("right-arrow-btn-li").addEventListener("click", imageScrollRightLI, false);
 
-  function imageScrollLeft() {
-    displayImg(curImage -= 1);
+  function imageScrollLeftLI() {
+    displayImgLI(curImageLI -= 1);
   }
 
-  function imageScrollRight() {
-    displayImg(curImage += 1);
+  function imageScrollRightLI() {
+    displayImgLI(curImageLI += 1);
   }
 
   //Display the current image
-  function currentImage(n) {
-    displayImg(curImage = n);
+  function currentImageLI(n) {
+    displayImgLI(curImageLI = n);
   }
 
-  function displayImg(n) {
+  function displayImgLI(n) {
     var i;
-    var images = document.getElementsByClassName('ind-project-img');
+    var images = document.getElementsByClassName('ind-project-img-li');
     var actualImages = document.getElementsByClassName('listit-img');
-    var circles = document.getElementsByClassName('circ');
+    var circles = document.getElementsByClassName('circ-li');
 
     if (n > images.length) {
-      curImage = 1;
+      curImageLI = 1;
     }
 
     if (n < 1) {
-      curImage = images.length;
+      curImageLI = images.length;
     }
 
     for (i = 0; i < images.length; i++) {
@@ -46,11 +44,62 @@ $(document).ready(function() {
         circles[i].className = circles[i].className.replace("active-circ", "");
     }
 
-    images[curImage-1].style.display = "inline-block";
-    images[curImage-1].style.margin = "0px -20px 0px -20px";
-    images[curImage-1].style.zindex = "-1";
-    actualImages[curImage-1].style.width = "90%";
-    actualImages[curImage-1].style.height = "80%";
-    circles[curImage-1].className += "fas fa-circle circ active-circ";
+    images[curImageLI-1].style.display = "inline-block";
+    images[curImageLI-1].style.margin = "0px -20px 0px -20px";
+    images[curImageLI-1].style.zindex = "-1";
+    actualImages[curImageLI-1].style.width = "90%";
+    actualImages[curImageLI-1].style.height = "80%";
+    circles[curImageLI-1].className += "fas fa-circle circ-li active-circ";
+  }
+
+  //PetTP image slideshow functions
+  var curImagePT = 1;
+  displayImgPT(curImagePT);
+
+  //Go to next image to right or left
+  document.getElementById("left-arrow-btn-pt").addEventListener("click", imageScrollLeftPT, false);
+  document.getElementById("right-arrow-btn-pt").addEventListener("click", imageScrollRightPT, false);
+
+  function imageScrollLeftPT() {
+    displayImgPT(curImagePT -= 1);
+  }
+
+  function imageScrollRightPT() {
+    displayImgPT(curImagePT += 1);
+  }
+
+  //Display the current image
+  function currentImagePT(n) {
+    displayImgPT(curImagePT = n);
+  }
+
+  function displayImgPT(n) {
+    var i;
+    var images = document.getElementsByClassName('ind-project-img-pt');
+    var actualImages = document.getElementsByClassName('pettp-img');
+    var circles = document.getElementsByClassName('circ-pt');
+
+    if (n > images.length) {
+      curImagePT = 1;
+    }
+
+    if (n < 1) {
+      curImagePT = images.length;
+    }
+
+    for (i = 0; i < images.length; i++) {
+        images[i].style.display = "none";
+    }
+
+    for (i = 0; i < circles.length; i++) {
+        circles[i].className = circles[i].className.replace("active-circ", "");
+    }
+
+    images[curImagePT-1].style.display = "inline-block";
+    images[curImagePT-1].style.margin = "0px -20px 0px -20px";
+    images[curImagePT-1].style.zindex = "-1";
+    actualImages[curImagePT-1].style.width = "90%";
+    actualImages[curImagePT-1].style.height = "80%";
+    circles[curImagePT-1].className += "fas fa-circle circ-pt active-circ";
   }
 });
