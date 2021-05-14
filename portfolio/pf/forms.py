962f763e5cdeb65ad10e01d8bd4d3b141f95e57d
mike1695 <luigixmario11695@gmail.com>
2021-05-14 16:11:29 -0400

    Installed pytest and fixed blog form

--- portfolio/pf/forms.py
+++ portfolio/pf/forms.py
@@ -4,11 +4,11 @@ from django.forms import ModelForm
 from pf.models import Catagory, Blog, Message, User
 
 #Form for creating a new blog catagory
-class BlogForm(UserCreationForm):
+class BlogForm(ModelForm):
     #form fields
     blogName = forms.CharField(max_length=100, required=True)
-    catagories = tags = forms.ModelMultipleChoiceField(queryset=Catagory.objects.all(),
-        widget=forms.CheckboxSelectMultiple, required=Treu, label="Catagories")
+    catagories = forms.ModelMultipleChoiceField(queryset=Catagory.objects.all(),
+        widget=forms.CheckboxSelectMultiple, required=True, label="Catagories")
 
     class Meta:
         model = Blog
