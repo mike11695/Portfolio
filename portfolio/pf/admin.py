0863bfdbf3dfca5b8aa3f58c9ce66496ec4e9153
mike1695 <luigixmario11695@gmail.com>
2021-05-19 18:59:25 -0400

    Began designing the blog list view page

--- portfolio/pf/admin.py
+++ portfolio/pf/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from pf.models import Catagory, Blog, Message
+
 # Register your models here.
+admin.site.register(Catagory)
+admin.site.register(Blog)
+admin.site.register(Message)
